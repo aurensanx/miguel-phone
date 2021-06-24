6879d145e8a85374abee56ab1a84063068909121
aaurensanz <aaurensanz@gmail.com>
2021-06-24 16:06:41 +0200

    Add styled-components

--- src/componentes/telefono/Acciones.js
+++ src/componentes/telefono/Acciones.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import { css } from 'styled-components'
 import { Pantalla } from '../commons/Pantalla'
+
 import { BotonAccion } from './BotonAccion'
+
+const llamarStyles = css`
+  background-color: #00886a;
+`
+const colgarStyles = css`
+  background-color: #b6002e;
+`
 
 export const Acciones = ({ numeroTelefono, llamarActivo, colgarActivo, llamar, colgar }) => (
   <div className="acciones">
     <Pantalla dato={numeroTelefono} tipo="numero"/>
-    <BotonAccion activo={llamarActivo} tipo="llamar" accion={llamar}>Llamar</BotonAccion>
-    <BotonAccion activo={colgarActivo} tipo="colgar" accion={colgar}>Colgar</BotonAccion>
+    <BotonAccion activo={llamarActivo} styles={llamarStyles} accion={llamar}>Llamar</BotonAccion>
+    <BotonAccion activo={colgarActivo} styles={colgarStyles} accion={colgar}>Colgar</BotonAccion>
   </div>
 )
--- src/componentes/telefono/BotonAccion.js
+++ src/componentes/telefono/BotonAccion.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
 
-export const BotonAccion = ({ children, activo, tipo, accion }) => (
-  <a className={`${activo ? tipo + ' activo' : tipo}`} onClick={accion}>
+const StyledAction = styled.a`
+  border-radius: 10px;
+  padding: 20px;
+  text-decoration: none;
+  color: inherit;
+  cursor: default;
+  opacity: 0.5;
+  transform: scale(0.9);
+  transition: all 0.2s ease-out;
+  ${({ activo }) => activo
+    ? `
+    cursor: pointer;
+    opacity: 1;
+    :hover {
+      transform: scale(1);
+    };
+  `
+: ''};
+  ${({ styles }) => styles || ''};
+`
+
+export const BotonAccion = ({ children, activo, styles, accion }) => (
+  <StyledAction activo={activo} styles={styles} onClick={accion}>
     {children}
-  </a>
+  </StyledAction>
 )
